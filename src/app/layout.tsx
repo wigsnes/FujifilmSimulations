475d0baeb1b175ca849a3c23e4fe405d6d14db3b
wigsnes <fredrik.wigsnes@gmail.com>
2024-01-27 23:41:27 +0100

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({
         {children}
         {modal}
         <div id="modal-root" />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
