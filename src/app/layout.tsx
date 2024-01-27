3d053cc26bb50697efe1ba7abe1c15ae6073d837
wigsnes <fredrik.wigsnes@gmail.com>
2024-01-27 23:37:25 +0100

    Include speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         {children}
         {modal}
         <div id="modal-root" />
+        <SpeedInsights />
       </body>
     </html>
   );
